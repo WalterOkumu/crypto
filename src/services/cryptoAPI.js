e58f18e4e4b6dbbbb0eee79cb2e5fd5ff280894d
WalterOkumu <okumu.oriaro@gmail.com>
2021-12-22 16:03:54 +0300

    Initial commit

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "8fc9800f7cmsh24a5ca2d9558d6ep1537c4jsn3a3138fd2d05"
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins")
+    })
+  })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
